198447dc00b7ece704abfb908eeb3dcb212189e1
yurashynkar <119353202+yurashynkar@users.noreply.github.com>
2023-02-11 13:00:43 +0300

    TicTacToeSwiftUI
    TicTacToeSwiftUI

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by юра шинкар on 1.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
